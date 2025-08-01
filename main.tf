12a674e0d027963e5b4e950b9bb250a89e35e1ec
bilbaose <bilbaose@amazon.es>
2025-08-01 13:06:13 +0200

    updated

--- main.tf
+++ main.tf
@@ -4,3 +4,16 @@
 module "create_breakglass_users" {
   source = "git::https://github.com/sebilo/maersk-lz-modules.git//organization_policies"
 }
+
+module "assign_permissionsets" {
+  source = "git::https://github.com/sebilo/maersk-lz-modules.git//sso_global_permissionsets_assignment"
+  account_assignments = [
+    {
+      account             = "417447013857",
+      permission_set_arn  = "arn:aws:sso:::permissionSet/ssoins-698724b2167b9294/ps-0704b729029e5657",
+      permission_set_name = "sso-gbl-lz-administrator",
+      principal_type      = "GROUP",
+      principal_name      = "LandingZone-Automation",
+    },
+  ]
+}
